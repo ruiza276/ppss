e292743564a633eea95bd8433bebbc34c3a40894
ruiza276 <ruizaa276@gmail.com>
2025-01-12 15:10:07 -0600

    WIP playing with chakra

---
+++ TODO.md
@@ -0,0 +1,3 @@
+1. You have now added Chakra UI as a component library
+   - https://www.chakra-ui.com/
+    -Let's look at this next to figure out the home page for ppss.
---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,28 @@
+import { Grid, GridItem } from "@chakra-ui/react"
+import { Image } from "@chakra-ui/react"
+
+ 
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <Grid templateColumns="repeat(4, 1fr)" gap="6">
+      <GridItem colSpan={2}>
+                <Image
+                  src="/ppss.png"
+                  alt="pps"
+                />
+      </GridItem>
+      <GridItem colSpan={1}>
+      <Image
+                  src="/ppss.png"
+                  alt="pps"
+                />
+      </GridItem>
+      <GridItem colSpan={1}>
+      <Image
+                  src="/ppss.png"
+                  alt="pps"
+                />
+      </GridItem>
+    </Grid>
+  );
+}
