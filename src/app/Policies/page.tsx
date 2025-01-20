414a6c98ba5fbe9ad6b6c8ed9426404cf5c9fe90
ruiza276 <ruizaa276@gmail.com>
2025-01-20 10:25:42 -0600

    policy page is up and new component

--- src/app/Policies/page.tsx
+++ src/app/Policies/page.tsx
@@ -1,3 +1,16 @@
+import { Image } from "@chakra-ui/react"
+
 export default function Page() {
-    return <p>Policies Page</p>;
+    return (     
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+          <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+            <Image
+              src="/policies.jpeg"
+              alt="rules"
+            />
+          </main>
+          <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+          </footer>
+        </div>
+        );
   }
